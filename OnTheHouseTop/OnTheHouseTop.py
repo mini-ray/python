be5a6edb0f7af75a9f8becddbb7ba3c933db991b
mini-ray <55558121+mini-ray@users.noreply.github.com>
2021-11-30 12:49:43 -0800

    Add files via upload

--- OnTheHouseTop/OnTheHouseTop.py
+++ OnTheHouseTop/OnTheHouseTop.py
@@ -1,1 +1,13 @@
+import playsound
+import schedule
+import time
 
+def song():
+	print("song")
+	playsound.playsound("OnTheHouseTop.mp3")
+	
+schedule.every().day.at("09:00").do(song)
+
+while 1:
+	schedule.run_pending()
+	time.sleep(1)
