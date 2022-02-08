515aeab0a1278a1d00c249a9a167d444d192f93a
mini-ray <55558121+mini-ray@users.noreply.github.com>
2022-02-08 11:03:03 -0800

    Add files via upload

---
+++ ProjectStem/code3.5.py
@@ -0,0 +1,11 @@
+g = int(input("What grade are you in? "))
+if g == 9:
+	print("Freshman")
+elif g == 10:
+	print("Sophomore")
+elif g == 11:
+	print("Junior")
+elif g == 12:
+	print("Senior")
+else:
+	print("Not in High School")
